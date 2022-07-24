aa02b495a66098843f98859b33b8bee1977336e6
dheerapat <dheerapat.t@kkumail.com>
2022-07-24 16:46:52 +0700

    add JWT finctionality to let user register and login

--- app.py
+++ app.py
@@ -3,14 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float
 import os
 from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
     os.path.join(basedir, 'planets.db')
+app.config['JWT_SECRET_KEY']= 'supersecret' # TODO: change this before deploy!
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+jwt = JWTManager(app)
 
 # create database via flask cli
 
@@ -94,6 +97,39 @@ def planets():
     planets_list = Planet.query.all()
     result = planets_schema.dump(planets_list)
     return jsonify(result)
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    email = request.form['email']
+    test = User.query.filter_by(email=email).first()
+    if test:
+        return jsonify(message='That email already taken.'), 409
+    else:
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        password = request.form['password']
+        user = User(first_name=first_name, last_name=last_name,
+                    email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify(message='User created.'), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.is_json:
+        email = request.json['email']
+        password = request.json['password']
+    else:
+        email = request.form['email']
+        password = request.form['password']
+    
+    test = User.query.filter_by(email=email, password=password).first()
+    if test:
+        access_token = create_access_token(identity=email)
+        return jsonify(message='Login succeeded', access_token=access_token)
+    else:
+        return jsonify(message='Bad E-mail or Password'), 401
 
 # database model
 
